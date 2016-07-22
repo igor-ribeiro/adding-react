f4fe9101d51cccf1fe93f0fcdc07ac932db6a9ad
Igor Ribeiro <igor.ribeiro.plus@gmail.com>
2016-07-22 19:02:44 -0300

    call react component from non-react environment

--- src/App.js
+++ src/App.js
@@ -1,18 +1,43 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-class App extends Component {
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isOpened: props.isOpened,
+    };
+
+    this.handleClose = this.handleClose.bind(this);
+  }
+
+  componentWillReceiveProps(props) {
+    this.setState({ ...props });
+  }
+
+  handleClose() {
+    this.setState({ isOpened: false });
+  }
+
   render() {
+    const isOpened = this.state.isOpened
+      ? 'flex'
+      : 'none';
+
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="App-modal" style={{display: isOpened}}>
+        <header className="App-modal-header">
+          Test {this.state.isOpened ? 'is-opened' : ''}
+        </header>
+
+        <section className="App-modal-body">
+          This is an amazing content for a modal!
+        </section>
+
+        <footer className="App-modal-footer">
+          <input className="App-modal-footer-action" onClick={this.handleClose} type="button" value="Close" />
+        </footer>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -3,7 +3,25 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+const button = document.querySelector('#open-modal');
+
+window.reactFakeState = {
+  modal: {
+    isOpened: false,
+  },
+};
+
+button.addEventListener('click', function (event) {
+  window.reactFakeState.modal.isOpened = true;
+
+  renderOrUpdateModal();
+});
+
+function renderOrUpdateModal () {
+  ReactDOM.render(
+    <App {...window.reactFakeState.modal} />,
+    document.getElementById('root')
+  );
+}
+
+renderOrUpdateModal();
